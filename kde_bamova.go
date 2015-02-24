bf1b766cd846725ed349a7f2a1de1849f4ea3bb9
RJ Nowling <rnowling@gmail.com>
2015-02-24 13:30:52 -0600

    Add function for reading occurrences

--- kde_bamova.go
+++ kde_bamova.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Hello, world.\n")
+
+	occur, _ := ReadOccurrences("../apple_bamovacomp_multinomial.txt")
+
+	fmt.Printf("Loci: %d, Populations: %d, Haplotypes: %d\n", occur.n_loci, occur.n_populations, occur.n_haplotypes)
 }
---
+++ kde_bamova_io.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+import "os"
+import "bufio"
+import "strings"
+import "strconv"
+
+type Occurrences struct {
+	n_loci int
+	n_populations int
+	n_haplotypes int
+
+	observed_counts *[][][]uint64
+}
+
+func ReadOccurrences(flname string) (*Occurrences, error) {
+	file, err := os.Open(flname)
+
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	populations := make([][]uint64, 0)
+
+	loci := make([][][]uint64, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		ln := scanner.Text()
+
+		// new locus
+		if strings.Contains(ln, "Marker") {
+			if len(populations) != 0 {
+				loci = append(loci, populations)
+				populations = make([][]uint64, 0)
+			}
+
+			continue
+		} else {
+			cols := strings.Split(ln, " ")
+
+			haplotype_counts := make([]uint64, len(cols) - 2)
+
+			for i, col := range cols[2:] {
+				value, _ := strconv.ParseUint(col, 10, 64)
+
+				haplotype_counts[i] = value
+			}
+
+			populations = append(populations, haplotype_counts)
+
+
+		}
+	}
+
+	fmt.Printf("Loci: %d, Populations: %d, Haplotypes: %d\n", len(loci), len(loci[0]), len(loci[0][0]))
+
+	occur := Occurrences{n_loci: len(loci), n_populations: len(loci[0]), n_haplotypes: len(loci[0][0]), observed_counts: &loci}
+
+	return &occur, err
+}
