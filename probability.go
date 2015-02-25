d96cbe5a39dda486852527722878212c746a4e81
RJ Nowling <rnowling@gmail.com>
2015-02-25 10:42:55 -0600

    Add gamma, gaussian samplers

--- probability.go
+++ probability.go
@@ -1,6 +1,7 @@
 package main
 
 import "math"
+import "math/rand"
 
 func logFactorial(n uint64) float64 {
 	if n == 0 || n == 1 {
@@ -44,4 +45,81 @@ func LogMultinomial(counts []uint64, probabilities []float64) float64 {
 	fact_num := logFactorial(total_count)
 
 	return fact_num + mult_log_prob - fact_denum
+}
+
+func SampleStandardNormal() float64 {
+	u1 := rand.Float64()
+	u2 := rand.Float64()
+
+	z0 := math.Sqrt(-2.0 * math.Log(u1)) * math.Cos(2.0 * math.Pi * u2)
+	//z1 := math.Sqrt(-2.0 * math.Log(u1)) * math.Sin(2.0 * math.Pi * u2)
+
+	return z0
+}
+
+func SampleGamma(shape float64, scale float64) float64 {
+	if shape < 1 {
+		return sampleGammaSmallShape(shape, scale)
+	} else {
+		return sampleGammaLargeShape(shape, scale)
+	}
+
+}
+
+func sampleGammaSmallShape(shape float64, scale float64) float64 {
+	for true {
+		u := rand.Float64()
+		bGS := 1.0 + shape / math.E
+		p := bGS * u
+
+		if (p <= 1.0) {
+			x := math.Pow(p, 1.0 / shape)
+			u2 := rand.Float64()
+
+			if u2 > math.Exp(-x) {
+				continue
+			} else {
+				return scale * x
+			}
+		} else {
+			x := -1.0 * math.Log((bGS - p) / shape)
+			u2 := rand.Float64()
+
+			if u2 > math.Pow(x, shape - 1.0) {
+				continue
+			} else {
+				return scale * x
+			}
+		}
+	}
+
+	return -1.0
+}
+
+func sampleGammaLargeShape(shape float64, scale float64) float64 {
+	d := shape - 0.333333333333333333
+	c := 1.0 / (3.0 * math.Sqrt(d))
+
+	for true {
+		x := rand.Float64() // TODO Gaussian
+		base := (1 + c * x)
+		v :=  base * base * base
+
+		if v <= 0 {
+			continue
+		} 
+
+		x2 := x * x
+		u := rand.Float64()
+
+		if u < 1.0 - 0.0331 * x2 * x2 {
+			return scale * d * v
+		}
+
+		if math.Log(u) < 0.5 * x2 + d * (1 - v + math.Log(v)) {
+			return scale * d * v
+		}
+	}
+
+	return -1.0
 }
